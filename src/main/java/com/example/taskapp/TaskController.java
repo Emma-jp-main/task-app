9081ff6d3c04ca818efc73d2df4a83a7446143b9
Emma-jp-main <mathlogue3431@gmail.com>
2026-04-26 20:48:18 +0900

    change from String to List<String>

--- src/main/java/com/example/taskapp/TaskController.java
+++ src/main/java/com/example/taskapp/TaskController.java
@@ -3,13 +3,17 @@ package com.example.taskapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 //「このクラスはAPIですよ」って宣言。JSONを返す
 @RestController
 public class TaskController {
 
-    //URLの後ろに/tasksと入るとokを返す
+    //URLの後ろに/tasksと入るとtaskのJSONを返す
+    //自動でJSON変換されてる
+    //これはSpring Bootが内部でやってる
     @GetMapping("/tasks")
-    public String test() {
-        return "ok";
+    public List<String> getTasks() {
+        return List.of("task1", "task2");
     }
 }
